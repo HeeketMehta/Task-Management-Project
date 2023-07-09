d5964513b8b0e3ee128ea7d56fb6efcf148f3f37
Heeket Mehta <heeketmehta@Heekets-MacBook-Pro.local>
2023-07-08 22:47:41 -0700

    authentication works perfectly

---
+++ server/index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const app = express();
+const authRoute = require("./Routes/AuthRoute");
+const cookieParser = require("cookie-parser");
+
+const PORT = 4000;
+const MONGO_URL = "mongodb+srv://task_manager:pass@cluster0.66uuoq3.mongodb.net/?retryWrites=true&w=majority";
+
+mongoose.connect(MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB is  connected successfully"))
+  .catch((err) => console.error(err));
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
+
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
+app.use(cookieParser());
+
+app.use(express.json());
+
+app.use("/", authRoute);
+// app.use(express.json());
+
+// app.listen(PORT, () =>{
+//     console.log(`SERVER IS RUNNING ON PORT ${PORT}`);
+// })
